98cd4989dc43588264ab9314b92b5ec007defb02
Razvan Radeanu <razvan.radeanu@cegeka.com>
2019-11-26 10:41:14 +0200

    Included dummy SPA

---
+++ Source/catalog/catalog.web/main.ts
@@ -0,0 +1,5 @@
+import 'reflect-metadata';
+
+import { AppModule } from 'src/app.module';
+
+AppModule.run();
---
+++ Source/catalog/catalog.web/src/framework/index.ts
@@ -0,0 +1,6 @@
+export * from './decorators';
+export * from './dom';
+export * from './markers';
+export * from './routing';
+export * from './forms';
+export * from './notifications';
