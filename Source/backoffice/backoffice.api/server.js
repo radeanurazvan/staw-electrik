7d457ccd971d3023343bf0a0fe269226e99fa8e6
Razvan <radeanu.razvan99@gmail.com>
2019-11-26 10:02:07 +0200

    Added docs and initial code structure

---
+++ Source/backoffice/backoffice.api/server.js
@@ -0,0 +1,40 @@
+class BatteryDefinition {
+    id;
+    name;
+    size;
+}
+
+class CatalogBattery {
+    id;
+    name;
+    size;
+    stock;
+    price;
+}
+
+class EnergyProviderDefinition {
+    id;
+    name;
+    coordinates;
+}
+
+class CatalogEnergyProvider {
+    id;
+    name;
+    coordinates;
+    pricePerUnit;
+}
+
+class Customer {
+    id;
+    name;
+    email;
+}
+
+
+class Order {
+    id;
+    batteries;
+    accumulators;
+    customerId;
+}
