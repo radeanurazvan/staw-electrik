ddabce5cd5a35138ab6b63ee2e9f0e72cb1c3c2f
Razvan <radeanu.razvan99@gmail.com>
2020-01-30 03:10:38 +0200

    energy catalog

--- Source/backoffice/backoffice.web/src/pages/energy/catalog/energy-catalog.page.ts
+++ Source/backoffice/backoffice.web/src/pages/energy/catalog/energy-catalog.page.ts
@@ -2,16 +2,21 @@ import { inject } from 'inversify';
 import { TrexPage, DomMaster, Page, OnInit } from '@framework';
 
 import * as template from './energy-catalog.page.html'
+import { EnergyService } from '../energy.service';
 
 @TrexPage({
     template: template
 })
 export class EnergyCatalogPage extends Page implements OnInit {
-    public constructor(@inject(DomMaster) master: DomMaster) {
+    public providers: any[] = [];
+
+    public constructor(
+        @inject(DomMaster) master: DomMaster,
+        @inject(EnergyService) private service:EnergyService) {
         super(master);
     }
 
-    public onInit(): void {
+    public async onInit() {
+        this.providers = await this.service.getCatalog();
     }
-
 }
